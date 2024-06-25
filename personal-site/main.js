79ddc3df283b30adab35a782536f6d90f98dd3f7
NeelKondapalli <neel2h06@gmail.com>
2024-06-25 08:46:26 -0700

    Added demo 3D site

---
+++ personal-site/main.js
@@ -0,0 +1,65 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+})
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth , window.innerHeight);
+camera.position.setZ(30);
+
+// renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry ( 10, 3, 16, 100 )
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347})
+const torus = new THREE.Mesh(geometry, material)
+scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff, 300)
+pointLight.position.set(1, 1, 1)
+scene.add(pointLight)
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 3)
+scene.add(ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24)
+  const material = new THREE.MeshStandardMaterial( { color: 0xffffff})
+  const star = new THREE.Mesh(geometry, material)
+
+  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+  star.position.set(x,y,z)
+  scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpg')
+scene.background = spaceTexture
+
+function animate() {
+  requestAnimationFrame( animate )
+
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.005
+  torus.rotation.z += 0.01
+
+  controls.update()
+
+  renderer.render(scene, camera)
+}
+
+animate()
